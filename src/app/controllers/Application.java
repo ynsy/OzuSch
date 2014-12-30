d4fc8fca87eab2116c703b5cc55dde65134a7993
ynsy <yilmaz.yunus58@gmail.com>
2014-12-30 12:12:49 +0200

    Shcedule

--- src/app/controllers/Application.java
+++ src/app/controllers/Application.java
@@ -49,52 +49,50 @@ public class Application extends Controller {
 			request());
 	public static ResultSet resultSet;
 	public static PreparedStatement statement;
-
+	public static ArrayList<models.JSONParser.Course> selectedCourse = new ArrayList<models.JSONParser.Course>();
 	public static String insertIntoCourses = "INSERT INTO courses (id) VALUES (1);";
 
 	public static Result index() throws ClassNotFoundException, SQLException,
 			FileNotFoundException, IOException, ParseException {
 
-//		models.JSONParser.Main.parseJSON();
-//		models.JSONParser.Course.addCoursesToDatabase();
-//		models.JSONParser.CourseInstructor.addInstructorsToDatabase();
-//		models.JSONParser.LectureInterval.addLectureIntervalsToDatabase();
+		// models.JSONParser.Main.parseJSON();
+		// models.JSONParser.Course.addCoursesToDatabase();
+		// models.JSONParser.CourseInstructor.addInstructorsToDatabase();
+		// models.JSONParser.LectureInterval.addLectureIntervalsToDatabase();
 
-//		return ok(homePage.render("deneme","home"));
-
+		// return ok(homePage.render("deneme","home"));
 
 		// After first connection please comment below line.
 
-//		Student.addUniversityToDatabase();
+		// Student.addUniversityToDatabase();
 
-		//retrieveStudentInformationFromDB();
-		//selectedCourses();
+		// retrieveStudentInformationFromDB();
+		// selectedCourses();
 		return ok(homePage.render(title, "home", url));
-			
 
 	}
-	
-	public static void retrieveStudentInformationFromDB() throws SQLException, ClassNotFoundException {
+
+	public static void retrieveStudentInformationFromDB() throws SQLException,
+			ClassNotFoundException {
 		DatabaseConnector.makeConnection();
 		statement = DatabaseConnector.connection
 				.prepareStatement("SELECT display_name, email FROM students");
 		resultSet = statement.executeQuery();
 
 		ResultSetMetaData rsmd = resultSet.getMetaData();
-	    int columnsNumber = rsmd.getColumnCount();
-	    while (resultSet.next()) {
-	        for (int i = 1; i <= columnsNumber; i++) {
-	            String columnValue = resultSet.getString(i);
-//	            System.out.print(columnValue);
-	            studentInformation.add(columnValue);
-	        }
-	 
-	    }
-	    System.out.println(studentInformation);
+		int columnsNumber = rsmd.getColumnCount();
+		while (resultSet.next()) {
+			for (int i = 1; i <= columnsNumber; i++) {
+				String columnValue = resultSet.getString(i);
+				// System.out.print(columnValue);
+				studentInformation.add(columnValue);
+			}
+
+		}
+		System.out.println(studentInformation);
 		statement.close();
 		resultSet.close();
 	}
-
 
 	public static Result offeredCourses() throws ClassNotFoundException,
 			SQLException, FileNotFoundException, IOException, ParseException {
@@ -108,141 +106,145 @@ public class Application extends Controller {
 	}
 
 	static Form<Courses> courseForm = Form.form(Courses.class);
-	
-	
-	public static Result selectedCourses() throws ClassNotFoundException, SQLException, FileNotFoundException, IOException, ParseException{
+
+	public static Result selectedCourses() throws ClassNotFoundException,
+			SQLException, FileNotFoundException, IOException, ParseException {
 
 		DatabaseConnector.makeConnection();
 		models.JSONParser.Course.retrieveCourseListFromDB();
 		Form<Courses> filledForm = courseForm.bindFromRequest();
-		
-		ArrayList<models.JSONParser.Course> selectedCourse = new ArrayList<models.JSONParser.Course>();
-		
-		courseList = models.JSONParser.Course.getDbCourseList();	
-	
+
+		courseList = models.JSONParser.Course.getDbCourseList();
+
 		String value = "";
-		
-		final Set<Map.Entry<String,String[]>> entries = request().queryString().entrySet();
-        
-		for (Map.Entry<String,String[]> entry : entries) {
-            final String key = entry.getKey();
-            value = Arrays.toString(entry.getValue());
-        }
-        value = value.substring(1);
-        value = value.substring(0, value.length()-1);
-       	
-       	StringTokenizer tokenizer = new StringTokenizer(value, ",");
-       	
-       	while(tokenizer.hasMoreTokens()){
-       		int token = Integer.parseInt(tokenizer.nextToken().trim());
-       		System.out.println("TOKEN:" + token);
-       		selectedCourse.add(controllers.Student.selectedCourses(token));
-       	}
-       
-		return ok(selectedCourses.render(title, "selectedCourses", url, selectedCourse));
+
+		final Set<Map.Entry<String, String[]>> entries = request()
+				.queryString().entrySet();
+
+		for (Map.Entry<String, String[]> entry : entries) {
+			final String key = entry.getKey();
+			value = Arrays.toString(entry.getValue());
+		}
+		value = value.substring(1);
+		value = value.substring(0, value.length() - 1);
+
+		StringTokenizer tokenizer = new StringTokenizer(value, ",");
+
+		while (tokenizer.hasMoreTokens()) {
+			int token = Integer.parseInt(tokenizer.nextToken().trim());
+			System.out.println("TOKEN:" + token);
+			selectedCourse.add(controllers.Student.selectedCourses(token));
+		}
+
+		return ok(selectedCourses.render(title, "selectedCourses", url,
+				selectedCourse));
 	}
-	
-	public static Result scheduler() throws ClassNotFoundException, SQLException{
-		
+
+	public static Result scheduler() throws ClassNotFoundException,
+			SQLException {
+
 		DatabaseConnector.makeConnection();
 		String value = "";
 		ArrayList<models.JSONParser.Course> selectedCourse = new ArrayList<models.JSONParser.Course>();
 		ArrayList<models.JSONParser.Course> finalCourses = new ArrayList<models.JSONParser.Course>();
-		
-		final Set<Map.Entry<String,String[]>> entries = request().queryString().entrySet();
-		
-		for (Map.Entry<String,String[]> entry : entries) {
-            final String key = entry.getKey();
-            value = Arrays.toString(entry.getValue());
-        }
+
+		final Set<Map.Entry<String, String[]>> entries = request()
+				.queryString().entrySet();
+
+		for (Map.Entry<String, String[]> entry : entries) {
+			final String key = entry.getKey();
+			value = Arrays.toString(entry.getValue());
+		}
 		value = value.substring(1);
-        value = value.substring(0, value.length()-1);
-        
-    	StringTokenizer tokenizer = new StringTokenizer(value, ",");
-        
-    	while(tokenizer.hasMoreTokens()){
-       		int token = Integer.parseInt(tokenizer.nextToken().trim());
-       		selectedCourse.add(controllers.Student.selectedCourses(token));
-       	}
-    	
-    	
-//    	for (models.JSONParser.Course course : selectedCourse) {
-//			ArrayList<LectureInterval> lectureIntervals = new ArrayList<LectureInterval>();
-//			lectureIntervals = course.getLectures();
-//			
-//			int startHour = 0;
-//			int startMinute = 0;
-//			int finishHour = 0;
-//			int finishMinute = 0;
-//			String day = "";
-//			
-//			if(finalCourses.isEmpty()){
-//				finalCourses.add(course);
-//			}else{
-//			    for (LectureInterval lectureInterval : lectureIntervals) {
-//			    	startHour = lectureInterval.getStartHour();
-//					startMinute = lectureInterval.getStartMinute();
-//					finishHour = lectureInterval.getFinishHour();
-//					finishMinute = lectureInterval.getFinishMinute();
-//					day = lectureInterval.getDay();
-//				}
-//				for (models.JSONParser.Course finalCourse : finalCourses) {
-//					ArrayList<LectureInterval> finalLectureInvervals = new ArrayList<LectureInterval>();
-//					finalLectureInvervals = finalCourse.getLectures();
-//					
-//					for (LectureInterval finalLectureInverval : finalLectureInvervals) {
-//						int finalStartHour = finalLectureInverval.getStartHour();
-//						int finalStartMinute = finalLectureInverval.getStartMinute();
-//						int finalFinishHour = finalLectureInverval.getFinishHour();
-//						int finalFinishMinute = finalLectureInverval.getFinishMinute();
-//						String finalDay = finalLectureInverval.getDay();
-//						
-//						if(day.equals(finalDay)){
-//							if()
-//						}else{
-//							finalCourses.
-//						}
-//						
-//					}
-//				}
-//			}
-//		}
-		
+		value = value.substring(0, value.length() - 1);
+
+		StringTokenizer tokenizer = new StringTokenizer(value, ",");
+
+		while (tokenizer.hasMoreTokens()) {
+			int token = Integer.parseInt(tokenizer.nextToken().trim());
+			selectedCourse.add(controllers.Student.selectedCourses(token));
+		}
+
+		// for (models.JSONParser.Course course : selectedCourse) {
+		// ArrayList<LectureInterval> lectureIntervals = new
+		// ArrayList<LectureInterval>();
+		// lectureIntervals = course.getLectures();
+		//
+		// int startHour = 0;
+		// int startMinute = 0;
+		// int finishHour = 0;
+		// int finishMinute = 0;
+		// String day = "";
+		//
+		// if(finalCourses.isEmpty()){
+		// finalCourses.add(course);
+		// }else{
+		// for (LectureInterval lectureInterval : lectureIntervals) {
+		// startHour = lectureInterval.getStartHour();
+		// startMinute = lectureInterval.getStartMinute();
+		// finishHour = lectureInterval.getFinishHour();
+		// finishMinute = lectureInterval.getFinishMinute();
+		// day = lectureInterval.getDay();
+		// }
+		// for (models.JSONParser.Course finalCourse : finalCourses) {
+		// ArrayList<LectureInterval> finalLectureInvervals = new
+		// ArrayList<LectureInterval>();
+		// finalLectureInvervals = finalCourse.getLectures();
+		//
+		// for (LectureInterval finalLectureInverval : finalLectureInvervals) {
+		// int finalStartHour = finalLectureInverval.getStartHour();
+		// int finalStartMinute = finalLectureInverval.getStartMinute();
+		// int finalFinishHour = finalLectureInverval.getFinishHour();
+		// int finalFinishMinute = finalLectureInverval.getFinishMinute();
+		// String finalDay = finalLectureInverval.getDay();
+		//
+		// if(day.equals(finalDay)){
+		// if()
+		// }else{
+		// finalCourses.
+		// }
+		//
+		// }
+		// }
+		// }
+		// }
+
 		return ok(scheduler.render(title, "selectedCourses", url, finalCourses));
 	}
 
 	static Form<Students> taskForm = Form.form(Students.class);
+
 	public static Result signUp() {
-		
+
 		return ok(signUpPage.render(title, "signUp", url,
 				"This is sign-up page. Please register to system.", taskForm));
 	}
-
 
 	public static Result login() {
 		return ok(loginPage.render(title, "login", url,
 				"This is login page. Please login to system."));
 	}
 
-	public static Result newStudent() throws Exception{
+	public static Result newStudent() throws Exception {
 		Form<Students> filledForm = taskForm.bindFromRequest();
 		String username = filledForm.data().get("username");
 		String password = filledForm.data().get("password");
 		String cpassword = filledForm.data().get("cpassword");
 		String email = filledForm.data().get("email");
 		String cemail = filledForm.data().get("cemail");
-		String message =  "";
-		if(email.contains(cemail) && password.contains(cpassword)){
-			
-			if(!Students.isEmailValid(email) && !Students.isUsernameValid(username)){
+		String message = "";
+		if (email.contains(cemail) && password.contains(cpassword)) {
 
-//				Universities university = new Universities();
-//				university.id = 1;
-//				university.name = "Ozyegin University";
-//				
-//				university.create(university);
-//
-//				
+			if (!Students.isEmailValid(email)
+					&& !Students.isUsernameValid(username)) {
+
+				// Universities university = new Universities();
+				// university.id = 1;
+				// university.name = "Ozyegin University";
+				//
+				// university.create(university);
+				//
+				//
 				Students student = new Students();
 				student.display_name = username;
 				student.password = password;
@@ -250,30 +252,30 @@ public class Application extends Controller {
 				student.university = new Universities();
 				student.university.id = 1;
 				student.university.name = "Ozyegin";
-			
 
-				if(Student.checkPasswordSatisfaction(password)){
+				if (Student.checkPasswordSatisfaction(password)) {
 					password = PasswordEncryption.mixPassword(password);
-					Student.addStudentToDatabase("", "", username, email, password);
+					Student.addStudentToDatabase("", "", username, email,
+							password);
 					message = "Successful";
-					controllers.Student.sendMail(email, Student.registerMailInfo);
-				}else{
+					controllers.Student.sendMail(email,
+							Student.registerMailInfo);
+				} else {
 					message = "Your password is weak";
 				}
-				
-				
 
-			}else{
+			} else {
 				message = "Your username or email is used";
 			}
-			
-		}else{
-				message = "Please enter correct values.";
+
+		} else {
+			message = "Please enter correct values.";
 		}
 
 		Boolean validEmail = Students.isEmailValid(email);
-		
-		return ok(signUpPage.render(title,"signUp",url,"mesaj: "+message, taskForm));
+
+		return ok(signUpPage.render(title, "signUp", url, "mesaj: " + message,
+				taskForm));
 	}
 
 	public void startScheduler(ArrayList<Course> usrCourseList) {
--- src/app/controllers/Scheduler.java
+++ src/app/controllers/Scheduler.java
@@ -4,18 +4,94 @@ import java.util.ArrayList;
 
 public class Scheduler {
 
-	ArrayList<Course> usrCourseList;
+	public static ArrayList<models.JSONParser.Course> selectedCoourseList = Application.selectedCourse;
+	public static ArrayList<Scheduler> schedules = new ArrayList<Scheduler>();
 
-	public Scheduler(ArrayList<Course> usrCourseList){
-		this.usrCourseList = usrCourseList;
+	public String courseName;
+	public String courseLocation;
+	public String time;
+	// public ArrayList<String> Monday = new ArrayList<String>();
+	// public ArrayList<String> Tuesday = new ArrayList<String>();
+	// public ArrayList<String> Wednesday = new ArrayList<String>();
+	// public ArrayList<String> Thursday = new ArrayList<String>();
+	// public ArrayList<String> Friday = new ArrayList<String>();
+
+	public String[] Monday = new String[15];
+	public String[] Tuesday = new String[15];
+	public String[] Wednesday = new String[15];
+	public String[] Thursday = new String[15];
+	public String[] Friday = new String[15];
+
+	public Scheduler(String courseName, String courseLocation, String time,
+			String[] Monday, String[] Tuesday, String[] Wednesday,
+			String[] Thursday, String[] Friday) {
+		this.courseName = courseName;
+		this.courseLocation = courseLocation;
+		this.time = time;
+		this.Monday = Monday;
+		this.Tuesday = Tuesday;
+		this.Wednesday = Wednesday;
+		this.Thursday = Thursday;
+		this.Friday = Friday;
+
 	}
-	
-	public void findCourse(String course){
-		
-		
+
+	public static void main(String[] args) {
+		for (int i = 0; i < selectedCoourseList.size(); i++) {
+			for (int j = i; i < selectedCoourseList.size(); j++) {
+				if (selectedCoourseList.get(i).getLectures().get(0).getDay() != selectedCoourseList
+						.get(j).getLectures().get(0).getDay()) {
+					if (selectedCoourseList.get(i).getLectures().get(0)
+							.getDay().equals("Monday")) {
+						for (int k = selectedCoourseList.get(i).getLectures()
+								.get(0).getStartHour(); k < selectedCoourseList
+								.get(i).getLectures().get(0).getFinishHour(); k++) {
+							schedules.get(i).Monday[k] = selectedCoourseList
+									.get(i).getDisplayName();
+						}
+					} else if (selectedCoourseList.get(i).getLectures().get(0)
+							.getDay().equals("Tuesday")) {
+						for (int k = selectedCoourseList.get(i).getLectures()
+								.get(0).getStartHour(); k < selectedCoourseList
+								.get(i).getLectures().get(0).getFinishHour(); k++) {
+							schedules.get(i).Tuesday[k] = selectedCoourseList
+									.get(i).getDisplayName();
+						}
+					} else if (selectedCoourseList.get(i).getLectures().get(0)
+							.getDay().equals("Wednesday")) {
+						for (int k = selectedCoourseList.get(i).getLectures()
+								.get(0).getStartHour(); k < selectedCoourseList
+								.get(i).getLectures().get(0).getFinishHour(); k++) {
+							schedules.get(i).Wednesday[k] = selectedCoourseList
+									.get(i).getDisplayName();
+						}
+					} else if (selectedCoourseList.get(i).getLectures().get(0)
+							.getDay().equals("Thursday")) {
+						for (int k = selectedCoourseList.get(i).getLectures()
+								.get(0).getStartHour(); k < selectedCoourseList
+								.get(i).getLectures().get(0).getFinishHour(); k++) {
+							schedules.get(i).Thursday[k] = selectedCoourseList
+									.get(i).getDisplayName();
+						}
+					} else if (selectedCoourseList.get(i).getLectures().get(0)
+							.getDay().equals("Friday")) {
+						for (int k = selectedCoourseList.get(i).getLectures()
+								.get(0).getStartHour(); k < selectedCoourseList
+								.get(i).getLectures().get(0).getFinishHour(); k++) {
+							schedules.get(i).Friday[k] = selectedCoourseList
+									.get(i).getDisplayName();
+						}
+					}
+				}
+			}
+		}
 	}
-	
-	public void eliminateCourses(){
-		
+
+	public static void takeIntervals(int courseIndex) {
+		for (int i = 0; i < selectedCoourseList.get(courseIndex).getLectures()
+				.size(); i++) {
+
+		}
 	}
+
 }
